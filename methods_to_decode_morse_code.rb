b725b5abce88989ba018e8647f25979d5b5f5985
lucascomamala <lucascomamala@gmail.com>
2023-04-05 13:39:27 -0600

    Method to decode a single word

--- methods_to_decode_morse_code.rb
+++ methods_to_decode_morse_code.rb
@@ -9,3 +9,12 @@ def decode_char(char)
    }
    morse_hash[char]
 end
+
+def decode_word(word)
+  decoded_word = ''
+  arr = word.split(' ')
+  arr.each { |c|
+    decoded_word += decode_char(c)
+  }
+  decoded_word
+end
