a8fa506732f06ec233f9bd788a5687a245b06e51
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-04-05 14:07:31 -0500

    Create decode_message

--- methods_to_decode_morse_code.rb
+++ methods_to_decode_morse_code.rb
@@ -1,20 +1,32 @@
 def decode_char(char)
-  morse_hash = { 
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', 
+  morse_hash = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
     '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
     '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
     '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
     '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
     '--..' => 'Z'
-   }
-   morse_hash[char]
+  }
+  morse_hash[char]
 end
 
 def decode_word(word)
   decoded_word = ''
-  arr = word.split(' ')
-  arr.each { |c|
+  arr = word.split
+  arr.each do |c|
     decoded_word += decode_char(c)
-  }
+  end
   decoded_word
 end
+
+def decode_message(string)
+  new_string = ''
+  string_to_array = string.split(/  /)
+  string_to_array.each do |word|
+    new_string += decode_word(word)
+    new_string += ' '
+  end
+  new_string
+end
+
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
